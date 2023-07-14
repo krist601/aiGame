e66dd68bde0011c31466a01fd39393cc3942fd6a
Kristian Sthefan Cortes Prieto <SoapyGenie@SoapyGenie.local>
2023-07-14 01:04:34 -0400

    feat: backgrounds

--- app/src/main/java/com/example/aigame/ui/presentation/QuestionFragment.kt
+++ app/src/main/java/com/example/aigame/ui/presentation/QuestionFragment.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -38,6 +39,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
@@ -130,22 +132,20 @@ class QuestionFragment : Fragment() {
                     .weight(1f)
             ) {
                 val question by viewModel.question.collectAsState()
-                    Surface(
-                        modifier = Modifier.height(300.dp).background(Color.Transparent),
+                    Box(
+                        modifier = Modifier.background(Color.Transparent).height(350.dp),
                     ) {
                         val painter = rememberImagePainter(data = imageUrl)
-                        Surface(
-                            modifier = Modifier.background(Color.Transparent).padding(24.dp),
-                        ) {
-                            Card() {
-                                Image(
-                                    painterResource(R.drawable.image_background),
-                                    modifier = Modifier.fillMaxSize()
-                                        .background(randomColor.first),
-                                    contentDescription = "",
-                                    contentScale = ContentScale.Crop
-                                )
-                            }
+
+                        Box(modifier = Modifier.background(Color.Transparent).padding(end = 32.dp, start = 32.dp, top = 48.dp, bottom = 32.dp)) {
+                            Image(
+                                painterResource(R.drawable.image_background),
+                                modifier = Modifier.fillMaxSize()
+                                    .background(Color.Transparent)
+                                    .clip(RoundedCornerShape(8)),
+                                contentDescription = "",
+                                contentScale = ContentScale.FillBounds
+                            )
                         }
                         Box(
                             modifier = Modifier
@@ -156,7 +156,7 @@ class QuestionFragment : Fragment() {
                                 contentDescription = null,
                                 contentScale = ContentScale.Fit,
                                 modifier = Modifier.fillMaxWidth()
-                                    .height(200.dp)
+                                    .height(250.dp)
                                     .align(Alignment.BottomCenter)
                             )
                         }
@@ -167,7 +167,6 @@ class QuestionFragment : Fragment() {
                             fontWeight = FontWeight.Bold,
                             modifier = Modifier
                                 .padding(16.dp)
-                                .align(Alignment.Start)
                         )
                 }
 
